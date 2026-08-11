5c6ca537dff19ec4fb3f7bbd9bb0118a3f78bdb4
manuelyilber-create <manuelyilber@gmail.com>
2026-08-10 23:31:10 -0400

    Update blockData.js

--- js/world/blockData.js
+++ js/world/blockData.js
@@ -10,19 +10,23 @@ export const BLOCKS = {
     IRON: 7,
     GOLD: 8,
     DIAMOND: 9,
-    TORCH: 10
+    TORCH: 10,
+    WATER: 11,
+    SAND: 12
 };
 
 export const BLOCK_PROPERTIES = {
     [BLOCKS.AIR]: { name: "Aire", solid: false, transparent: true },
-    [BLOCKS.DIRT]: { name: "Tierra Orgánica", solid: true, color: "#543821", detailColor: "#3d2715" },
+    [BLOCKS.DIRT]: { name: "Tierra Orgánica", solid: true, color: "#543821" },
     [BLOCKS.GRASS]: { name: "Pasto Nutritivo", solid: true, color: "#427a24", topColor: "#57a62e" },
-    [BLOCKS.STONE]: { name: "Piedra Rocosa", solid: true, color: "#4d5156", detailColor: "#383a3d", hardness: 3 },
+    [BLOCKS.STONE]: { name: "Piedra Rocosa", solid: true, color: "#4d5156" },
     [BLOCKS.WOOD]: { name: "Madera de Roble", solid: true, color: "#614126", barkColor: "#3b2614" },
     [BLOCKS.LEAVES]: { name: "Follaje Frondoso", solid: true, color: "#2e6924", transparent: true },
     [BLOCKS.COAL]: { name: "Mena de Carbón", solid: true, color: "#4d5156", spotColor: "#1a1a1b" },
     [BLOCKS.IRON]: { name: "Mena de Hierro", solid: true, color: "#4d5156", spotColor: "#c2916b" },
     [BLOCKS.GOLD]: { name: "Mena de Oro", solid: true, color: "#4d5156", spotColor: "#e6bf35" },
-    [BLOCKS.DIAMOND]: { name: "Mena de Diamante", solid: true, color: "#4d5156", spotColor: "#2be0d4", light: 0.2 },
-    [BLOCKS.TORCH]: { name: "Antorcha Intensa", solid: false, light: 1.0, color: "#ff9900" }
+    [BLOCKS.DIAMOND]: { name: "Mena de Diamante", solid: true, color: "#4d5156", spotColor: "#2be0d4" },
+    [BLOCKS.TORCH]: { name: "Antorcha Intensa", solid: false, color: "#ff9900" },
+    [BLOCKS.WATER]: { name: "Agua Cristalina", solid: false, transparent: true, color: "rgba(30, 144, 255, 0.6)" },
+    [BLOCKS.SAND]: { name: "Arena Fina", solid: true, color: "#d2b48c" }
 };
