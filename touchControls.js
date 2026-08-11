7ba05105bf2a4d5284dcc23d0992cb69b82b2f1c
manuelyilber-create <manuelyilber@gmail.com>
2026-08-11 00:57:52 -0400

    Create touchControls.js

---
+++ touchControls.js
@@ -0,0 +1,11 @@
+// js/ui/touchControls.js
+export class TouchControls {
+    constructor(canvas, keys) {
+        this.canvas = canvas;
+        this.keys = keys;
+    }
+
+    render(ctx) {
+        // Renderizador base de controles táctiles
+    }
+}
